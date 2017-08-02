207e22d20001346d9d86a9a339cbcdb168817129
Vitor Gomes Miranda <vigmiranda@gmail.com>
2017-08-01 23:10:09 -0300

    Test structure in react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import App2 from './App2';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App2 />, document.getElementById('root2'));
 registerServiceWorker();
